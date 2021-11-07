3918d0e0170bb4e0f214e0d16aaba8f0863fa34a
asdfghjkxd <73705042+asdfghjkxd@users.noreply.github.com>
2021-11-07 22:39:18 +0800

    Implemented Other Data Extraction Classes

--- pages/opensea.py
+++ pages/opensea.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-from pages.classes.opensea_class import OpeanseaAssets
+from pages.classes.opensea_class import OpeanseaAssets, OpenseaEvents, OpenseaCollections, OpenseaBundles
 import pages.config.opensea_config as default
 
 
@@ -15,49 +15,75 @@ def app():
     default.RETRIEVAL_METHOD = st.selectbox('Select Data Retrieval Type',
                                             ('Assets', 'Events', 'Collections', 'Bundles'))
 
-    st.markdown('## Flags\n'
-                '### Scraper Behaviour')
-    default.GET_ALL = st.checkbox('Scrape Maximum API Returns?', value=True)
-    if not default.GET_ALL:
-        if st.checkbox('Define owner Parameter', value=False):
-            default.OWNER = st.text_input('Key in Address of Owner of Asset')
-        if st.checkbox('Define token_ids Parameter', value=False):
-            default.TOKEN_IDS = st.text_input('Key in Array of Token IDs to Search')
-        if st.checkbox('Define asset_contract_addresses Parameter', value=False):
-            default.ASSET_CONTRACT_ADDRESSES = st.text_input('Key in NFT Contract Address for Assets of Search')
-        if st.checkbox('Define order_by Parameter', value=False):
-            default.ORDER_BY = st.selectbox('Select Order', ('token_id', 'sale_date', 'sale_count'))
-        if st.checkbox('Define order_direction Parameter', value=False):
+    # ------------------------------------------------------------------------------------------------------------ #
+    #                                                  App Modes                                                   #
+    # ------------------------------------------------------------------------------------------------------------ #
+    if default.RETRIEVAL_METHOD == 'Assets':
+        # define flags
+        st.markdown('## Flags\n'
+                    '### Scraper Behaviour')
+        default.GET_ALL = st.checkbox('Scrape Maximum API Returns?', value=True)
+        if not default.GET_ALL:
+            if st.checkbox('Define owner Parameter', value=False):
+                default.OWNER = st.text_input('Key in Address of Owner of Asset')
+
+            if st.checkbox('Define token_ids Parameter', value=False):
+                default.TOKEN_IDS = st.text_input('Key in Array of Token IDs to Search')
+
+            if st.checkbox('Define asset_contract_addresses Parameter', value=False):
+                default.ASSET_CONTRACT_ADDRESSES = st.text_input('Key in NFT Contract Address for Assets of Search',
+                                                                 help='Note that if you wish to key in a list into the '
+                                                                      'input box, delimit your addresses with commas.')
+                if len(default.ASSET_CONTRACT_ADDRESSES) != 0:
+                    st.info('Addresses Detected!')
+                else:
+                    st.warning(
+                        'Warning, No Address Detected. Proceeding in this state will result in a None value being '
+                        'passed into asset_contract_addresses parameter.')
+            if st.checkbox('Define order_by Parameter', value=False):
+                default.ORDER_BY = st.selectbox('Select Order', ('token_id', 'sale_date', 'sale_count'))
+
+            if st.checkbox('Define offset Parameter', value=True):
+                default.OFFSET = st.number_input('Offset',
+                                                 min_value=0,
+                                                 max_value=10000,
+                                                 value=0)
+
+            if st.checkbox('Define limit Parameter', value=True):
+                default.LIMIT = st.number_input('Limit',
+                                                min_value=1,
+                                                max_value=50,
+                                                value=20)
+
+            if st.checkbox('Define collection Parameter', value=False):
+                default.COLLECTION = st.text_input('Limit responses to members of a Collection')
+
+        if st.checkbox('Define order_direction Parameter', value=True):
             default.ORDER_DIRECTION = st.selectbox('Select Direction', ('asc', 'desc'))
-        if st.checkbox('Define offset Parameter', value=False):
-            default.OFFSET = st.number_input('Offset',
-                                             min_value=0,
-                                             max_value=10000,
-                                             value=0)
-        if st.checkbox('Define limit Parameter', value=False):
-            default.LIMIT = st.number_input('Limit',
-                                            min_value=1,
-                                            max_value=50,
-                                            value=20)
-        if st.checkbox('Define collection Parameter', value=False):
-            default.COLLECTION = st.text_input('Limit responses to members of a Collection')
+
         if st.checkbox('Define API Key', value=False):
             default.API_KEY = st.text_input('API Key')
 
-    st.markdown('### App Behaviour')
-    default.SAVE = st.checkbox('Save Outputs?')
-    default.VERBOSE = st.checkbox('Display Outputs?')
-    if default.VERBOSE:
-        default.VERBOSITY = st.number_input('Number of Datapoints to Display?',
-                                            min_value=0,
-                                            max_value=10000,
-                                            value=20)
+        st.markdown('### App Behaviour')
+        default.SAVE = st.checkbox('Save Outputs?', value=True)
+        default.VERBOSE = st.checkbox('Display Outputs?', value=False)
+        if default.VERBOSE:
+            default.VERBOSITY = st.number_input('Number of Datapoints to Display?',
+                                                min_value=0,
+                                                max_value=10000,
+                                                value=20)
 
-    # app mode
-    if default.RETRIEVAL_METHOD == 'Assets':
+        # start extraction
         st.markdown('## Asset Retrieval\n'
                     'Ensure that the parameters you wish to pass into the class is loaded successfully.')
+
         if st.button('Proceed with Data Extraction'):
+            # process the input addresses
+            if default.ASSET_CONTRACT_ADDRESSES is not None and len(default.ASSET_CONTRACT_ADDRESSES) != 0:
+                default.ASSET_CONTRACT_ADDRESSES = set(address.strip() for address in default.ASSET_CONTRACT_ADDRESSES.
+                                                       split(sep=','))
+                st.info(f'Addresses Loaded: {[address for address in default.ASSET_CONTRACT_ADDRESSES]}')
+
             try:
                 if default.GET_ALL:
                     default.OFFSET = 0
@@ -109,3 +135,12 @@ def app():
                         st.dataframe(df.head(default.VERBOSITY))
                     else:
                         st.dataframe(df)
+
+    elif default.RETRIEVAL_METHOD == 'Events':
+        pass
+
+    elif default.RETRIEVAL_METHOD == 'Collections':
+        pass
+
+    elif default.RETRIEVAL_METHOD == 'Bundles':
+        pass
