39679a6068d122acb8465f8a0713d8dd7db2e46d
asdfghjkxd <73705042+asdfghjkxd@users.noreply.github.com>
2021-11-07 22:38:08 +0800

    Implemented Other Data Extraction Classes

--- pages/config/opensea_config.py
+++ pages/config/opensea_config.py
@@ -1,13 +1,24 @@
 # change any of the parameters below to alter the default behaviour of the scraper
-OWNER = None
-TOKEN_IDS = None
+ACCOUNT_ADDRESS = None
+API_KEY = None
+ASSET_CONTRACT_ADDRESS = None
 ASSET_CONTRACT_ADDRESSES = None
+ASSET_OWNER = None
+AUCTION_TYPE = None
+COLLECTION = None
+COLLECTION_SLUG = None
+EVENT_TYPE = None
+LIMIT = 20
+OCCURRED_AFTER = None
+OCCURRED_BEFORE = None
+OFFSET = 0
+ON_SALE = None
+ONLY_OPENSEA = False
 ORDER_BY = None
 ORDER_DIRECTION = 'desc'
-OFFSET = 0
-LIMIT = 20
-COLLECTION = None
-API_KEY = None
+OWNER = None
+TOKEN_ID = None
+TOKEN_IDS = None
 RETRIEVAL_METHOD = 'Assets'
 GET_ALL = True
 
