1bdd30a38ee874e6e3c1b457e923393c21cce633
asdfghjkxd <73705042+asdfghjkxd@users.noreply.github.com>
2021-11-13 16:03:20 +0800

    Extended Suport to ImmutableX

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 # CUSTOM PAGE IMPORTS
 import streamlit as st
 from multipage import MultiPage
-from pages import opensea
+from pages import opensea, immutablex
 
 # INSTANTIATE THE APP
 app = MultiPage()
 
 # DEFINE THE PAGES AND THE APPS THEY CONTAIN
 app.add_page('Opensea', opensea.app)
+app.add_page('ImmutableX', immutablex.app)
 
 # RUN THE APP
 app.run()
