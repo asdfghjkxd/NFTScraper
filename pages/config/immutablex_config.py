71e1e4d0cc90c23baa297056dbc39e1f690d3970
asdfghjkxd <73705042+asdfghjkxd@users.noreply.github.com>
2021-11-13 16:04:40 +0800

    Extended Support to ImmutableX

---
+++ pages/config/immutablex_config.py
@@ -0,0 +1,32 @@
+# change any of the parameters below to alter the default behaviour of the scraper
+ADDRESS = None
+BLACKLIST = None
+BUY_ORDERS = None
+COLLECTION = None
+CURSOR = None
+DIRECTION = None
+INCLUDE_FEES = None
+METADATA = None
+NAME = None
+NEXT_PAGE_TOKEN = None
+ORDER_BY = None
+PAGE_SIZE = None
+SELL_ORDERS = None
+STATUS = None
+SYMBOL = None
+TOKEN_ADDRESS = None
+TOKEN_ID = None
+UPDATED_MAX_TIMESTAMP = None
+UPDATED_MIN_TIMESTAMP = None
+USER = None
+
+# change any of the following to modify the behavior of the app
+GET_ALL = True
+RETRIEVAL_METHOD = 'Assets'
+VERBOSE = True
+SAVE = True
+VERBOSITY = 20
+ADVANCED = False
+QUERY_MODE = ''
+QUERY_PARAMS = None
+ASSERT_INPUTS = False
