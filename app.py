f48b87b043b90323ddca3b25b4202e5e40ee25eb
asdfghjkxd <73705042+asdfghjkxd@users.noreply.github.com>
2021-11-14 13:23:22 +0800

    Extended Support to Mintable and Rarible

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 # CUSTOM PAGE IMPORTS
 import streamlit as st
 from multipage import MultiPage
-from pages import opensea, immutablex
+from pages import opensea, immutablex, mintable, rarible
 
 # INSTANTIATE THE APP
 app = MultiPage()
@@ -9,6 +9,8 @@ app = MultiPage()
 # DEFINE THE PAGES AND THE APPS THEY CONTAIN
 app.add_page('Opensea', opensea.app)
 app.add_page('ImmutableX', immutablex.app)
+app.add_page('Mintable', mintable.app)
+app.add_page('Rarible', rarible.app)
 
 # RUN THE APP
 app.run()
